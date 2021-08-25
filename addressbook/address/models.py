0561f5e13335a93469f2c234b8108554e2dcb36e
Kiren Kaur <kaurkir@umail.iu.edu>
2021-08-25 17:14:26 -0400

    able to add new users now and store their data

--- addressbook/address/models.py
+++ addressbook/address/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+   firstName = models.CharField(max_length=500)
+   lastName = models.CharField(max_length=500)
+   phoneNumber = models.IntegerField(max_length=10)
+   emailAddress = models.EmailField(max_length=500)
+   streetAddress = models.CharField(max_length=150)
+   
+   def __str__(self):
+      return self.firstName
--- addressbook/address/views.py
+++ addressbook/address/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
 # Create your views here.
 def index(request):
    return render(request, 'index.html')
+
+def addContact(request):
+   if request.method == "POST":
+      new_contact = Contact(
+         first_name = request.POST['firstname'],
+         last_name = request.POST['lastname'],
+         phoen_number = request.POST['phonenumber'],
+         email_address = request.POST['emailaddress'],
+         street_address = request.POST['streetaddress']
+      )
+      new_contact.save()
+      return redirect('/')
+   return render(request, 'new-contact.html')
